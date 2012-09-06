129d8e1a01ed33ed2d9a03e9a71cde30dc88c176
jaredbischof <jbischof@gmail.com>
2012-09-06 14:35:16 -0500

    Loosened criteria of URL check to allow missing "http://"

--- Html/MetaZen.js
+++ Html/MetaZen.js
@@ -255,7 +255,7 @@ function isDate(s) {
 }
 
 function isUrl(s) {
-  var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+  var regexp = /(ftp|http|https)?:?\/?\/?(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
   return regexp.test(s);
 }
 
