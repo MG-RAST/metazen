e883e862ce979a1eba8a8fa5a668a1356d0a8830
Jared Bischof <jbischof@gmail.com>
2012-10-29 12:27:46 -0500

    Moved configuration variables into this file.

---
+++ conf/Conf.pm
@@ -0,0 +1,10 @@
+package Conf;
+
+#####################################################################
+# Metazen configuration
+#####################################################################
+
+$app_id       = 'MetaZen';
+$app_secret   = '4igdibXTeSznDiuDAdd5BgKCL';
+$oAuth_url    = 'http://metagenomics.anl.gov/oAuthPPO.cgi';
+$redirect_url = 'http://metagenomics.anl.gov/metazen.cgi';
