c1d9a54cac0f9c1cc8c6c9f974ee3b1b55e1f14f
Jared Bischof <jbischof@gmail.com>
2013-10-17 14:51:52 -0500

    Added template for Metazen configuration variables.

---
+++ conf/metazen_config_template.pm
@@ -0,0 +1,12 @@
+package metazen_config;
+
+#####################################################################
+# Metazen configuration
+#####################################################################
+
+$mz_app_id       = '';
+$mz_app_secret   = '';
+$mz_oAuth_url    = '';
+$mz_redirect_url = '';
+
+$mz_google_analytics = "";
